2fa78d84ae85a200d8927718f58e6c7a669c8bf8
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2021-04-05 17:56:52 -0300

    Fin seccion 6 - IngresosEgresosApp - funcionalidades basicas

---
+++ 04-ingresoEgresoApp/src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estadistica',
+  templateUrl: './estadistica.component.html',
+  styleUrls: ['./estadistica.component.scss']
+})
+export class EstadisticaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ 04-ingresoEgresoApp/src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+  styleUrls: ['./ingreso-egreso.component.scss']
+})
+export class IngresoEgresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
